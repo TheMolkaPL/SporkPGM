e61098a1ce44d12958692e27af75d33397b84cf6
RainoBoy97 <rainoboy97@gmail.com>
2014-03-20 10:35:30 +0100

    C:/Program Files (x86)/Git/rotation and /next

--- src/main/java/io/sporkpgm/commands/UserCommands.java
+++ src/main/java/io/sporkpgm/commands/UserCommands.java
@@ -13,13 +13,28 @@ import com.sk89q.minecraft.util.commands.CommandException;
 
 public class UserCommands {
 
-	@Command(aliases = { "maps" }, desc = "Lists all the loaded maps on the server", anyFlags = false, min = 0, max = 1)
+	@Command(aliases = { "listmaps", "maps" }, desc = "Lists all the loaded maps on the server", anyFlags = false, min = 0, max = 1)
 	public static void maps(CommandContext args, CommandSender sender) throws CommandException {
 		new PaginatedResult<SporkMap>("Loaded maps") {
 			public String format(SporkMap map, int index) {
 				return (index + 1) + ". " + ChatColor.GOLD + map.getName();
 			}
 		}.display(sender, Spork.get().getMapManager().getMaps(), args.getInteger(0, 1));;
+	}
+
+	@Command(aliases = { "rotation", "rot" }, desc = "Lists all the maps in the rotation", anyFlags = false, min = 0, max = 1)
+	public static void rotation(CommandContext args, CommandSender sender) throws CommandException {
+		new PaginatedResult<SporkMap>("Rotation") {
+			public String format(SporkMap map, int index) {
+				return (index + 1) + ". " + ChatColor.GOLD + map.getName();
+			}
+		}.display(sender, Spork.get().getMatchManager().getRotation(), args.getInteger(0, 1));;
+	}
+
+	@Command(aliases = { "nextmap", "next", "nm" }, desc = "Shows the next map played", anyFlags = false, min = 0, max = 0)
+	public static void next(CommandContext args, CommandSender sender) throws CommandException {
+		sender.sendMessage(ChatColor.DARK_PURPLE + "Next map: " + ChatColor.GOLD + Spork.get().getMatchManager().getNextMap().getName());
+
 	}
 
 }
