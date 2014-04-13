30790c7472926ab21ce75fe14e5734e290e1b802
ParaPenguin <harry.fox.ic@icloud.com>
2014-04-13 09:10:11 +0100

    Add Item Remove

--- src/main/java/io/sporkpgm/Spork.java
+++ src/main/java/io/sporkpgm/Spork.java
@@ -26,6 +26,7 @@ import io.sporkpgm.module.builder.Builder;
 import io.sporkpgm.module.exceptions.ModuleLoadException;
 import io.sporkpgm.module.modules.damage.DisableDamageBuilder;
 import io.sporkpgm.module.modules.friendlyfire.FriendlyFireBuilder;
+import io.sporkpgm.module.modules.itemremove.ItemRemoveBuilder;
 import io.sporkpgm.module.modules.maxheight.MaxHeightBuilder;
 import io.sporkpgm.module.modules.mob.MobBuilder;
 import io.sporkpgm.objective.core.CoreBuilder;
@@ -338,6 +339,7 @@ public class Spork extends JavaPlugin {
 		builders.add(DisableDamageBuilder.class);
 		builders.add(FriendlyFireBuilder.class);
 		builders.add(MaxHeightBuilder.class);
+		builders.add(ItemRemoveBuilder.class);
 	}
 
 	public Rotation getRotation() {
