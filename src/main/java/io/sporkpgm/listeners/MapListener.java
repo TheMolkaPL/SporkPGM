a81f5e3a686d69281553724d3af98f32ba566770
ParaPenguin <harry.fox.ic@icloud.com>
2014-04-13 06:41:52 +0100

    Lock and prevent any block changes
    While matches aren't running

--- src/main/java/io/sporkpgm/listeners/MapListener.java
+++ src/main/java/io/sporkpgm/listeners/MapListener.java
@@ -1,19 +1,23 @@
 package io.sporkpgm.listeners;
 
 import io.sporkpgm.map.event.BlockChangeEvent;
+import io.sporkpgm.match.Match;
 import io.sporkpgm.region.Region;
 import io.sporkpgm.rotation.RotationSlot;
 import io.sporkpgm.team.spawns.SporkSpawn;
 import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 public class MapListener implements Listener {
 
-	@EventHandler
+	@EventHandler(priority = EventPriority.LOWEST)
 	public void onBlockChange(BlockChangeEvent event) {
-		if(isSpawn(event.getLocation()))
+		if(!Match.getMatch().isRunning()) {
 			event.setCancelled(true);
+			event.setLocked(true);
+		}
 	}
 
 	public boolean isSpawn(Location location) {
